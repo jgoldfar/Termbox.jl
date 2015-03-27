133c14fd70bedb87bd9af19496d4aac61720759d
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-03-27 16:46:15 -0400

    Update to README, and include wrapper in Termbox.jl

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Termbox
 
 [![Build Status](https://travis-ci.org/jgoldfar/Termbox.jl.svg?branch=master)](https://travis-ci.org/jgoldfar/Termbox.jl)
+
+This is a wrapper for [Termbox](https://github.com/nsf/termbox) in Julia. More documentation coming...
--- src/Termbox.jl
+++ src/Termbox.jl
@@ -1,5 +1,11 @@
+# Termbox.jl: Wrapper around the Termbox library (https://github.com/nsf/termbox)
 module Termbox
+if isfile(joinpath(dirname(dirname(@__FILE__)), "deps", "deps.jl"))
+  include(joinpath(dirname(dirname(@__FILE__)), "deps", "deps.jl"))
+else
+  error("Termbox not properly installed. Please run Pkg.build(\"Termbox\")")
+end
 
-# package code goes here
+include("termbox_h.jl")
 
 end # module
