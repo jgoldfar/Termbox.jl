3bb9562bae2ed5f2b9200befae8c0532f7b97b9c
Jonathan Goldfarb <jgoldfar@gmail.com>
2017-09-28 21:29:37 -0400

    Build against Github release

--- deps/build.jl
+++ deps/build.jl
@@ -3,16 +3,15 @@ using Compat
 
 @BinDeps.setup
 
-libtermbox = library_dependency("libtermbox", aliases=["termbox"])
+const libtermbox = library_dependency("libtermbox", aliases=["termbox"])
 
 provides(Sources,
-         URI("https://bitbucket.org/jgoldfar/termbox-jl/downloads/termbox-master.tar.gz"),
-         libtermbox)
+         URI("https://github.com/nsf/termbox/archive/v1.1.0.tar.gz"),
+         libtermbox,
+         unpacked_dir = "termbox-1.1.0")
 
-# println(BinDeps.depsdir(libtermbox))
-prefix = joinpath(BinDeps.depsdir(libtermbox), "usr")
-srcdir = joinpath(BinDeps.depsdir(libtermbox), "src", "termbox-master")
-# println("Installing TermBox source to ", srcdir)
+const prefix = joinpath(BinDeps.depsdir(libtermbox), "usr")
+const srcdir = joinpath(BinDeps.depsdir(libtermbox), "src", "termbox-1.1.0")
 
 provides(SimpleBuild, (@build_steps begin
                          GetSources(libtermbox)
